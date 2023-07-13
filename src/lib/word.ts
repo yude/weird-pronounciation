7b086a0c9bccba5b66565723f506ab69f15eea09
yude <i@yude.jp>
2023-07-13 09:03:17 +0900

    fix: Update directory structure

--- src/lib/word.ts
+++ src/lib/word.ts
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 
 export async function getWords() {
-    const filePath = path.join(process.cwd(), "words.json");
+    const filePath = path.join(process.cwd(), "words/words.json");
 
     const res = fs.readFileSync(filePath, "utf8");
     const words = JSON.parse(res);

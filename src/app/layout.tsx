0b7ece351c2d1b0877667470aa817c50dc3fdf17
yude <i@yude.jp>
2023-07-13 09:00:18 +0900

    fix: Return to top page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import Link from "next/link";
+
 export const metadata = {
   title: "異常発音",
   description: "Weird pronounciation database",
@@ -14,7 +16,9 @@ export default function RootLayout({
     <html lang="ja">
       <body className="max-w-2xl mx-auto mt-2">
         <div className="text-center font-serif bg-pink-200/75 text-4xl tracking-wide decoration-pink-500 decoration-4">
-          <p style={{ transform: `scaleX(2.9)` }}>異常発音</p>
+          <p style={{ transform: `scaleX(2.9)` }}>
+            <Link href="/">異常発音</Link>
+          </p>
         </div>
         {children}
       </body>

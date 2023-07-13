f68b5f43285d586f71f451bcc9a44873f11f5758
yude <i@yude.jp>
2023-07-13 09:00:31 +0900

    feat: Add detail page

--- src/app/components/Words.tsx
+++ src/app/components/Words.tsx
@@ -1,10 +1,7 @@
-import { getWords } from "@/lib/word";
+import Link from "next/link";
 
-type Word = {
-  word: string;
-  pronounciation: string;
-  audio: string;
-};
+import { getWords } from "@/lib/word";
+import { Word } from "@/types/word";
 
 export default async function WordList() {
   const words = await getWords();
@@ -14,7 +11,9 @@ export default async function WordList() {
       {words.map((word: Word) => (
         <div key={word.word} className="text-center inline-block m-2">
           <p className="font-serif">{word.pronounciation}</p>
-          <p className="font-mono text-3xl">{word.word}</p>
+          <p className="font-mono text-3xl">
+            <Link href={`/word/${word.word}`}>{word.word}</Link>
+          </p>
         </div>
       ))}
     </div>
---
+++ src/app/word/[id]/page.tsx
@@ -0,0 +1,38 @@
+import { getWords } from "@/lib/word";
+import { Word } from "@/types/word";
+import Share from "@/app/components/Share";
+
+export async function generateStaticParams() {
+  const words = await getWords();
+  return words;
+}
+
+type WordPageParams = {
+  id: string;
+};
+
+export default async function WordPage({ params }: { params: WordPageParams }) {
+  const words = await getWords();
+  const query = params.id.replace("%20", " ");
+  const word = words.find(
+    (row: Word) => encodeURIComponent(row.word) === params.id
+  );
+
+  const this_url = "https://weird-pronounciation.vercel.app/word/" + params.id;
+  const this_title = `異常発音 - ${word.word} (${word.pronounciation})`;
+
+  if (word && typeof word.pronounciation !== "undefined") {
+    return (
+      <div className="mt-5">
+        <p className="text-center font-serif text-2xl">{word.pronounciation}</p>
+        <p className="text-center font-mono text-6xl">{word.word}</p>
+        <div className="text-center">
+          <Share title={this_title} url={this_url} />
+        </div>
+        <div className="font-serif mt-1">{word.description}</div>
+      </div>
+    );
+  } else {
+    return <p>Not found. Requested word: {encodeURIComponent(params.id)}</p>;
+  }
+}
